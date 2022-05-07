b6a1246f49362fae856922c6bfa37687728189c3
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-05-07 15:29:17 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -19,41 +19,68 @@ void printArray(int arr[], int size)
 class Solution
 {
     public:
-    void merge(int a[], int l, int m, int r)
+    void merge(int a[], int l, int mid, int r)
     {
          // Your code here
-        int x[m-l+1],y[r-m];
-    	for(int i=0;i<m-l+1;i++)
-        	x[i]=a[l+i];
-    	for(int i=0;i<r-m;i++)
-        	y[i]=a[m+i+1];
-    	int i=0,j=0,k=l;
-    	while(i<m-l+1 && j<r-m)
-    	{
-    		if(x[i]<=y[j])
-    		{
-    			a[k]=x[i];
-    			i++;
-    		}
-    		else
-    		{
-    			a[k]=y[j];
-    			j++;
-    		}
-    		k++;
-    	}
-    	while(i<m-l+1)
-    	{
-    		a[k]=x[i];
-    		i++;
-    		k++;
-    	}
-    	while(j<r-m)
-    	{
-    		a[k]=y[j];
-    		j++;
-    		k++;
-    	}
+    //     int x[m-l+1],y[r-m];
+    // 	for(int i=0;i<m-l+1;i++)
+    //     	x[i]=a[l+i];
+    // 	for(int i=0;i<r-m;i++)
+    //     	y[i]=a[m+i+1];
+    // 	int i=0,j=0,k=l;
+    // 	while(i<m-l+1 && j<r-m)
+    // 	{
+    // 		if(x[i]<=y[j])
+    // 		{
+    // 			a[k]=x[i];
+    // 			i++;
+    // 		}
+    // 		else
+    // 		{
+    // 			a[k]=y[j];
+    // 			j++;
+    // 		}
+    // 		k++;
+    // 	}
+    // 	while(i<m-l+1)
+    // 	{
+    // 		a[k]=x[i];
+    // 		i++;
+    // 		k++;
+    // 	}
+    // 	while(j<r-m)
+    // 	{
+    // 		a[k]=y[j];
+    // 		j++;
+    // 		k++;
+    // 	}
+        vector<int> v;
+        for(int i=l;i<=r;i++) {
+            v.push_back(a[i]);
+        }
+        int idx=l;
+        int st1=0,st2=mid-l+1;
+        while(st1<mid-l+1 and st2<r-l+1) {
+            if(v[st1]<=v[st2]) {
+                a[idx]=v[st1];
+                st1++;
+            }
+            else {
+                a[idx]=v[st2];
+                st2++;
+            }
+            idx++;
+        }
+        while(st1<mid-l+1) {
+            a[idx]=v[st1];
+            st1++;
+            idx++;
+        }
+        while(st2<r-l+1) {
+            a[idx]=v[st2];
+            st2++;
+            idx++;
+        }
     }
     public:
     void mergeSort(int a[], int l, int r)
