569656e562e4e304969fbb294519bf8b3b5eacbd
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-06-03 13:49:23 +0530

    Time: 782 ms (50.51%), Space: 111.9 MB (53.18%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,76 +1,69 @@
 class Solution {
 public:
-    struct segtree {
-        vector<long long> t;
-        long long sz;
-        segtree(long long n) {
-            t.resize(4*n+5);
-            sz=n;
-        }
-        void update(long long st,long long en,long long v,long long idx,long long val) {
-            if(st>en or idx<st or idx>en) {
-                return;
-            } 
-            if(st==en) {
-                t[v]=val;
-                return;
-            }
-            long long mid=(st+en)/2;
-            update(st,mid,2*v,idx,val);
-            update(mid+1,en,2*v+1,idx,val);
-            t[v]=t[2*v]+t[2*v+1];
-        }
-        void update(long long idx,long long val) {
-            update(0,sz-1,1,idx,val);
-        }
-        long long getSum(long long st,long long en,long long v,long long l,long long r) {
-            if(st>en or l>r or l>en or r<st) {
-                return 0;
-            }
-            if(l<=st and r>=en) {
-                return t[v];
-            }
-            long long mid=(st+en)/2;
-            return (getSum(st,mid,2*v,l,r)+getSum(mid+1,en,2*v+1,l,r));
-        }
-        long long getSum(long long l,long long r) {
-            return getSum(0,sz-1,1,l,r);
-        }
-    };
     
-    int reversePairs(vector<int>& nums) {
-        long long n=nums.size();
-        vector<pair<long long,long long>> arr;
-        for(long long i=0;i<n;i++) {
-            arr.push_back({nums[i],i});
-        }
-        sort(arr.begin(),arr.end());
-        long long num=1;
-        vector<long long> mp(n);
-        for(long long i=0;i<n;i++) {
-            long long j=i;
-            while(j<n and arr[j].first==arr[i].first) {
-                mp[arr[j].second]=num;
+    
+    
+    int merge(vector<int> &arr,int l,int mid,int r) {
+        vector<int> temp(r-l+1);
+        int idx1=l,idx2=mid,idx=0,ans=0;
+        int i=l,j=mid;
+        
+        auto top = [&] (int x,int y) {
+            int res=x/y;
+            if(x%y) {
+                res++;
+            }
+            return res;
+        };
+        
+        for(int i=l;i<mid;i++) {
+            while(j<=r and arr[i]>2ll*arr[j]) {
                 j++;
             }
-            j--;
-            i=j;
-            num++;
+            ans+=(j-mid);
         }
-        segtree st(num);
-        long long ans=0;
-        for(long long i=0;i<n;i++) {
-            long z=nums[i],mx=arr[n-1].first,mn=arr[0].first;
-            z*=2ll;
-            pair<long long, long long> p1 = {2ll*nums[i]+1ll,0ll};
-            auto it=lower_bound(arr.begin(),arr.end(),p1);
-            if(it!=arr.end()) {
-                auto p=*it;
-                long long idx=p.second;
-                ans+=st.getSum(mp[idx],num-1);
+        while(idx1<mid and idx2<=r) {
+            if(arr[idx1]<=arr[idx2]) {
+                temp[idx]=arr[idx1];
+                idx++;
+                idx1++;
             }
-            st.update(mp[i],st.getSum(mp[i],mp[i])+1);
+            else {
+                temp[idx]=arr[idx2];
+                idx++;
+                idx2++;
+            }
+        }
+        while(idx1<mid) {
+            temp[idx]=arr[idx1];
+            idx++;
+            idx1++;
+        }
+        while(idx2<=r) {
+            temp[idx]=arr[idx2];
+            idx++;
+            idx2++;
+        }
+        for(int i=l;i<=r;i++) {
+            arr[i]=temp[i-l];
         }
         return ans;
+    }
+    
+    int countMergeSort(vector<int> &arr,int l,int r) {
+        if(l>=r) {
+            return 0;
+        }
+        int mid=(l+r)/2;
+        int ans=0;
+        ans+=countMergeSort(arr,l,mid);
+        ans+=countMergeSort(arr,mid+1,r);
+        ans+=merge(arr,l,mid+1,r);
+        return ans;
+    }
+    
+    int reversePairs(vector<int>& nums) {
+        int n=nums.size();
+        return countMergeSort(nums,0,n-1);
     }
 };
