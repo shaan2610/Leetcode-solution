e5057fc7713ce532efb31cb85862866f2f2c76be
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-06-08 15:50:04 +0530

    Time: 23 ms (25.51%), Space: 7 MB (95.90%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,63 +1,54 @@
 class Solution {
 public:
-    bool ispossible(int row,int col,vector<string> &board) {
-        int n=board.size();
-        for(int i=0;i<n;i++) {
-            if(board[i][col]=='Q') {
-                return false;
-            }
-        }
-        for(int j=0;j<n;j++) {
-            if(board[row][j]=='Q') {
-                return false;
-            }
+    
+    void solve(int col,int n,vector<string> &board,vector<vector<string>> &ans) {
+        
+        if(col==n) {
+            ans.push_back(board);
+            return;
         }
         
-        auto chkbound = [&] (int x,int y) {
+        auto chkBound = [&] (int x,int y) {
             if(x>=0 and x<n and y>=0 and y<n) {
                 return true;
             }
             return false;
         };
         
-        int dx[] = {1,1,-1,-1};
-        int dy[] = {1,-1,1,-1};
-        for(int dir=0;dir<4;dir++) {
-            int x=row+dx[dir],y=col+dy[dir];
-            while(chkbound(x,y)) {
-                if(board[x][y]=='Q') {
-                    return false;
+        auto isPossible = [&] (int r) {
+            int c=col;
+            int dx[] = {-1,-1,-1,0,0,1,1,1};
+            int dy[] = {-1,0,1,-1,1,-1,0,1};
+            for(int dir=0;dir<8;dir++) {
+                int x=r+dx[dir],y=c+dy[dir];
+                while(chkBound(x,y)) {
+                    if(board[x][y]=='Q') {
+                        return false;
+                    }
+                    x+=dx[dir];
+                    y+=dy[dir];
                 }
-                x+=dx[dir];
-                y+=dy[dir];
             }
-        }
-        return true;
-    } 
-    
-    void helper(int col,vector<string> &board,vector<vector<string>> &ans) {
-        int n=board.size();
-        if(col==n) {
-            ans.push_back(board);
-            return;
-        }
+            return true;
+        };
+        
         for(int row=0;row<n;row++) {
-            if(ispossible(row,col,board)) {
+            if(isPossible(row)) {
                 board[row][col]='Q';
-                helper(col+1,board,ans);
+                solve(col+1,n,board,ans);
                 board[row][col]='.';
             }
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
         string s;
         for(int i=0;i<n;i++) {
             s.push_back('.');
         }
         vector<string> board(n,s);
-        helper(0,board,ans);
+        vector<vector<string>> ans;
+        solve(0,n,board,ans);
         return ans;
     }
 };
