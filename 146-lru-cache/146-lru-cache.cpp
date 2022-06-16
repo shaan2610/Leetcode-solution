9b16a6f6a7bc00cfe93375c1102a353870448db4
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-06-16 18:03:22 +0530

    Time: 553 ms (64.98%), Space: 181.4 MB (6.68%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,78 @@
+class LRUCache {
+public:
+    
+    class Node {
+    public:
+        int key,value;
+        Node* next;
+        Node* prev;
+        Node(int ky,int val) {
+            key=ky;
+            value=val;
+            next=NULL;
+            prev=NULL;
+        }
+    };
+    
+    unordered_map<int,Node*> mp;
+    int cap,curr,timer;
+    Node *head = new Node(-1,-1);
+    Node *tail = new Node(-1,-1);
+    
+    LRUCache(int capacity) {
+        cap=capacity;
+        curr=0;
+        head->next=tail;
+        tail->prev=head;
+//         head->tail
+    }
+    
+    void insertNode(int key,int value) {
+        Node* newNode=new Node(key,value);
+        newNode->next=head->next;
+        head->next=newNode;
+        newNode->next->prev=newNode;
+        newNode->prev=head;
+        mp[key]=newNode;
+    }
+    
+    void deleteNode(Node* &node) {
+        node->prev->next=node->next;
+        node->next->prev=node->prev;
+    }
+    
+    int get(int key) {
+        if(mp.count(key)) {
+            int ans=mp[key]->value;
+            deleteNode(mp[key]);
+            mp.erase(key);
+            insertNode(key,ans);
+            return ans;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.count(key)) {
+            deleteNode(mp[key]);
+            mp.erase(key);
+            insertNode(key,value);
+        }
+        else if(curr<cap) {
+            insertNode(key,value);
+            curr++;
+        }
+        else {
+            mp.erase(tail->prev->key);
+            deleteNode(tail->prev);
+            insertNode(key,value);
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
