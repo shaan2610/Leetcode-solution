304165db97cae66d79118674d42b7588304a42fa
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-06-18 12:20:06 +0530

    Time: 39 ms (9.03%), Space: 8.3 MB (20.30%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n=s.size();
+        map<char,int> m;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        int ans=m[s[n-1]];
+        for(int i=n-2;i>=0;i--) {
+            if(m[s[i]]<m[s[i+1]]) {
+                ans-=m[s[i]];
+            }
+            else {
+                ans+=m[s[i]];
+            }
+        }
+        return ans;
+    }
+};
