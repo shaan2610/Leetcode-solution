3fa534e5ad0d4171de0e41cc4d3bc62a8b16946b
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-06-22 10:20:05 +0530

    Time: 8 ms (55.96%), Space: 16.2 MB (93.67%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,49 +11,17 @@
  */
 class Solution {
 public:
+    bool helper(TreeNode* curr1,TreeNode* curr2) {
+        if(not curr1 or not curr2) {
+            return (curr1==curr2);
+        }
+        return (helper(curr1->left,curr2->right) and helper(curr1->right,curr2->left) and curr1->val==curr2->val);
+    }
+    
     bool isSymmetric(TreeNode* root) {
         if(not root) {
             return true;
         }
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-        int chk=0;
-        while(not q.empty()) {
-            int z=q.size();
-            vector<pair<int,int>> v;
-            while(z--) {
-                auto p=q.front();
-                TreeNode* node=p.first;
-                int deviation=p.second;
-                q.pop();
-                v.push_back({node->val,deviation});
-                if(node->left) {
-                    q.push({node->left,deviation-1});
-                }
-                if(node->right) {
-                    q.push({node->right,deviation+1});
-                }
-            }
-            int sz=v.size();
-            int i=0,j=sz-1;
-            if(sz&1) {
-                if(chk==0) {
-                    chk++;
-                }
-                else {
-                    return false;
-                }
-            }
-            while(i<=j) {
-                if(v[i].first==v[j].first and v[i].second+v[j].second==0) {
-                    i++;
-                    j--;
-                }
-                else {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return helper(root->left,root->right);
     }
 };
