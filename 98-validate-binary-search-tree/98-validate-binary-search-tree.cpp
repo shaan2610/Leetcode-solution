985cc94f3e942d966356728164b408c12d9707c1
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-06-24 16:01:22 +0530

    Time: 18 ms (55.23%), Space: 22 MB (6.39%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,20 +11,30 @@
  */
 class Solution {
 public:
-    bool isBST(TreeNode* curr,long mn,long mx) {
+    
+    const long inf=1e10;
+    
+    bool isBST(TreeNode* curr,long &mn,long &mx) {
         if(not curr) {
             return true;
         }
-        if(curr->val>=mn and curr->val<=mx) {
-            return (isBST(curr->left,mn,curr->val-1l) and isBST(curr->right,curr->val+1l,mx));
-        }
-        else {
+        long lmn=inf,lmx=-inf,val=curr->val;
+        bool left=isBST(curr->left,lmn,lmx);
+        if(lmx>=curr->val or not left) {
             return false;
         }
+        long rmn=inf,rmx=-inf;
+        bool right=isBST(curr->right,rmn,rmx);
+        if(rmn<=curr->val or not right) {
+            return false;
+        }
+        mn=min({mn,val,lmn,rmn});
+        mx=max({mx,val,lmx,rmx});
+        return true;
     }
     
     bool isValidBST(TreeNode* root) {
-        long mx=INT_MAX,mn=INT_MIN;
+        long mn=inf,mx=-inf;
         return isBST(root,mn,mx);
     }
 };
