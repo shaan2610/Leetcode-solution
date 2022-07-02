176c987a62af38277035788046e155f00f234e48
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-07-02 19:58:21 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -5,25 +5,65 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 		
+	const int N=1e5+5;	
+		
+	class segtree {
+	public:
+	    vector<int> t;
+	    int n;
+	    segtree(int sz) {
+	        n=sz;
+	        t.resize(4*n+5);
+	    }
+	    
+	    void update(int st,int en,int v,int idx,int val) {
+	        if(st>en or idx>en or idx<st) {
+	            return;
+	        }
+	        if(st==en) {
+	            t[v]=val;
+	            return;
+	        }
+	        int mid=(st+en)/2;
+	        update(st,mid,2*v,idx,val);
+	        update(mid+1,en,2*v+1,idx,val);
+	        t[v]=max(t[2*v],t[2*v+1]);
+	    }
+	    
+	    void update(int idx,int val) {
+	        update(0,n-1,1,idx,val);
+	    }
+	    
+	    int getMax(int st,int en,int v,int l,int r) {
+	        if(st>en or l>r or r<st or l>en) {
+	            return 0;
+	        }
+	        if(st>=l and en<=r) {
+	            return t[v];
+	        }
+	        int mid=(st+en)/2;
+	        return max(getMax(st,mid,2*v,l,r),getMax(mid+1,en,2*v+1,l,r));
+	    }
+	    
+	    int getMax(int l,int r) {
+	        return getMax(0,n-1,1,l,r);
+	    }
+	    
+	};
 
 	public:
 	int maxSumIS(int arr[], int n)  
 	{  
 	    // Your code goes here
-	    vector<int> dp(n);
-	    dp[0]=arr[0];
-	    int ans=dp[0];
-	    for(int i=1;i<n;i++) {
-	        dp[i]=arr[i];
-	        for(int j=0;j<i;j++) {
-	            if(arr[j]<arr[i]) {
-	                dp[i]=max(dp[i],dp[j]+arr[i]);
-	            }
-	        }
-	        ans=max(ans,dp[i]);
+	    segtree st(N);
+	    int ans=0;
+	    for(int i=0;i<n;i++) {
+	        int val=arr[i]+st.getMax(0,arr[i]-1);
+	        ans=max(ans,val);
+	        st.update(arr[i],max(val,st.getMax(arr[i],arr[i])));
 	    }
 	    return ans;
-	}  
+	}
 };
 
 // { Driver Code Starts.
