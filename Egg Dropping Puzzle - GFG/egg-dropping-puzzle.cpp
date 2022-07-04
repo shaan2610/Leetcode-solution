2c03bf04eb000e80e669ba3e568abae6887162d3
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-07-05 00:39:02 +0530

    Added solution - LeetHub

--- Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -11,19 +11,18 @@ class Solution
     int eggDrop(int n, int k) 
     {
         // your code here
-        vector<vector<int>> dp(k+1,vector<int>(n+1));
+        vector<vector<long long>> dp(k+1,vector<long long>(n+1));
         for(int i=0;i<=k;i++) {
-            dp[i][1]=i;
+            dp[i][0]=INT_MAX;
         }
         for(int j=0;j<=n;j++) {
-            dp[1][j]=1;
+            dp[0][j]=0;
         }
-        dp[1][0]=INT_MAX;
-        for(int i=2;i<=k;i++) {
-            for(int j=2;j<=n;j++) {
+        for(int i=1;i<=k;i++) {
+            for(int j=1;j<=n;j++) {
                 dp[i][j]=INT_MAX;
                 for(int x=1;x<=i;x++) {
-                    dp[i][j]=min(dp[i][j],max(dp[x-1][j-1],dp[i-x][j])+1);
+                    dp[i][j]=min(dp[i][j],max(dp[x-1][j-1],dp[i-x][j])+1ll);
                 }
             }
         }
