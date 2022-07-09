583b7001d0bb5f459f9605da57ae66a728ff385d
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2022-07-09 08:04:32 +0530

    Time: 522 ms (80.55%), Space: 181.4 MB (6.24%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,71 +1,83 @@
-class LRUCache {
+class Node {
 public:
-    
-    class Node {
-    public:
-        int key,value;
-        Node* next;
-        Node* prev;
-        Node(int ky,int val) {
-            key=ky;
-            value=val;
-            next=NULL;
-            prev=NULL;
-        }
-    };
-    
+    Node* next;
+    Node* prev;
+    int data,key;
+    Node(int key,int data) {
+        this->data=data;
+        this->key=key;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+
+class LRUCache {
+private:
+    Node* head;
+    Node* tail;
+    int capacity;
     unordered_map<int,Node*> mp;
-    int cap,curr,timer;
-    Node *head = new Node(-1,-1);
-    Node *tail = new Node(-1,-1);
-    
+    int currSize;
+public:
     LRUCache(int capacity) {
-        cap=capacity;
-        curr=0;
+        this->capacity=capacity;
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
         head->next=tail;
         tail->prev=head;
-//         head->tail
+        currSize=0;
     }
     
-    void insertNode(int key,int value) {
-        Node* newNode=new Node(key,value);
-        newNode->next=head->next;
-        head->next=newNode;
-        newNode->next->prev=newNode;
-        newNode->prev=head;
-        mp[key]=newNode;
+    bool isEmpty() {
+        return (currSize==0);
     }
     
-    void deleteNode(Node* &node,int key) {
-        node->prev->next=node->next;
-        node->next->prev=node->prev;
-        mp.erase(key);
+    bool isFull() {
+        return (currSize==capacity);
+    }
+    
+    void insert(int key,int val) {
+        Node* node = new Node(key,val);
+        mp[key]=node;
+        node->next=head->next;
+        node->prev=head;
+        head->next=node;
+        node->next->prev=node;
+        currSize++;
+    }
+    
+    void erase(int key) {
+        if(mp.count(key)) {
+            Node* node=mp[key];
+            node->prev->next=node->next;
+            node->next->prev=node->prev;
+            mp.erase(key);
+            currSize--;
+        }
+    }
+    
+    void eraseLRU() {
+        erase(tail->prev->key);
     }
     
     int get(int key) {
-        if(mp.count(key)) {
-            int ans=mp[key]->value;
-            deleteNode(mp[key],key);
-            insertNode(key,ans);
-            return ans;
+        if(not mp.count(key)) {
+            return -1;
         }
-        return -1;
+        int ans=mp[key]->data;
+        erase(key);
+        insert(key,ans);
+        return ans;
     }
     
     void put(int key, int value) {
-        if(mp.count(key)) {
-            deleteNode(mp[key],key);
-            insertNode(key,value);
+        erase(key);
+        if(isFull()) {
+            eraseLRU();
         }
-        else if(curr<cap) {
-            insertNode(key,value);
-            curr++;
-        }
-        else {
-            deleteNode(tail->prev,tail->prev->key);
-            insertNode(key,value);
-        }
+        insert(key,value);
     }
+    
 };
 
 /**
