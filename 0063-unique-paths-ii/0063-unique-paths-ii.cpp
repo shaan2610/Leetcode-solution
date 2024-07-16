ab27b455251f21d9a07b638845d8ca8de8b3fe9d
Shaan Kumar <58791448+shaan2610@users.noreply.github.com>
2024-07-16 11:05:05 +0530

    Time: 5 ms (36.53%), Space: 10.3 MB (13.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,7 +5,8 @@ public:
         // dp[i][j] --> no of ways to reach cell (i, j) from (0, 0)
         // ans --> dp[n-1][m-1]
         
-        vector<vector<int>> dp(n, vector<int>(m));
+        // vector<vector<int>> dp(n, vector<int>(m));
+        vector<int> new_dp(m), old_dp(m);
         
         // Corner cases
         // If starting point has an obstacle or dest have an obstacle
@@ -14,23 +15,28 @@ public:
         }
         
         // Base case
-        dp[0][0] = 1;
+        new_dp[0] = 1;
         
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 // Checking if there is an obstacle in the current cell
                 if(obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
+                    new_dp[j] = 0;
                     continue;
                 }
+                if(i == 0 and j == 0) {
+                    continue;
+                }
+                new_dp[j] = 0;
                 if(i - 1 >= 0) {
-                    dp[i][j] += dp[i-1][j];
+                    new_dp[j] += old_dp[j];
                 }
                 if(j - 1 >= 0) {
-                    dp[i][j] += dp[i][j-1];
+                    new_dp[j] += new_dp[j-1];
                 }
             }
+            swap(new_dp, old_dp);
         }
-        return dp[n-1][m-1];
+        return old_dp[m-1];
     }
 };
